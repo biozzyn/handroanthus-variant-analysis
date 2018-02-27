383416d271accc803daf319668d7296668306949
biozzyn <36853158+biozzyn@users.noreply.github.com>
2018-02-27 18:51:20 -0300

    Update setup.sh

--- data/setup.sh
+++ data/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+## NEED TO BE MODIFIED BY THE ANALYST
+IGGMC="<path-to-IGGMC-analysis-tool>"   #need to setup; see README.md
+DATADIR="<absolute-path-to-data>" 	#need to setup; see README.md
+##
+
 # Check that external tools are accessible:
 FQDUMP=`which fastq-dump`;
 TAIL=`which tail`;
 CAT=`which cat`;
+TOUCH=`which touch`;
 MKDIR=`which mkdir`
 
 if [[ -z "${FQDUMP}" || ! -x "${FQDUMP}" ]]; then
@@ -18,24 +24,34 @@ elif [[ -z "${CAT}" || ! -x "${CAT}" ]]; then
 elif [[ -z "${MKDIR}" || ! -x "${MKDIR}" ]]; then
     error 127 "mkdir not in PATH env variable or not executable";
 
+elif [[ -z "${TOUCH}" || ! -x "${TOUCH}" ]]; then
+    error 127 "mkdir not in PATH env variable or not executable";
+
 fi
 
-SAMPLE="sra_metadata_acc-himp-1r-processed-ok.tsv"
+if [[ ! -e "${DATADIR}/data.complete" ]]; then
+    echo "executing" >&2;
 
-[ -d "HIMP1_1" ] || ${MKDIR} "HIMP1_1";
+    SAMPLE="${DATADIR}/sra_metadata_acc-himp-1r-processed-ok.tsv"
 
-cat ${SAMPLE} | ${TAIL} -n +2 | while IFS=$'\t' read accession study object_status bioproject_accession biosample_accession library_ID title library_strategy library_source library_selection library_layout platform instrument_model design_description filetype filename; do
+    [ -d "${DATADIR}/HIMP1_1" ] || ${MKDIR} "${DATADIR}/HIMP1_1";
+
+    cat ${SAMPLE} | ${TAIL} -n +2 | while IFS=$'\t' read accession study object_status bioproject_accession biosample_accession library_ID title library_strategy library_source library_selection library_layout platform instrument_model design_description filetype filename; do
 	library_ID=`echo $library_ID | awk '{split($1,lib,"-"); print lib[1]""lib[2]}'`
-	echo "fastq-dump --gzip -Z -A $accession > HIMP1_1/HIMP1_1-${library_ID}_0.fastq.gz"
+	echo "${FQDUMP} --gzip -Z -A $accession > ${DATADIR}/HIMP1_1/HIMP1_1-${library_ID}_0.fastq.gz";
+    done >data.batch 
 
-done
+    SAMPLE="${DATADIR}/sra_metadata_acc-himp-2-processed-ok.tsv"
 
-SAMPLE="sra_metadata_acc-himp-2-processed-ok.tsv"
+    [ -d "HIMP2_1" ] || ${MKDIR} "HIMP2_1";
 
-[ -d "HIMP2_1" ] || ${MKDIR} "HIMP2_1";
-
-cat ${SAMPLE} | ${TAIL} -n +2 | while IFS=$'\t' read accession study object_status bioproject_accession biosample_accession library_ID title library_strategy library_source library_selection library_layout platform instrument_model design_description filetype filename; do
+    cat ${SAMPLE} | ${TAIL} -n +2 | while IFS=$'\t' read accession study object_status bioproject_accession biosample_accession library_ID title library_strategy library_source library_selection library_layout platform instrument_model design_description filetype filename; do
 	library_ID=`echo $library_ID | awk '{split($1,lib,"-"); print lib[1]""lib[2]}'`
-	echo "fastq-dump --gzip -Z -A $accession > HIMP2_1/HIMP2_1-${library_ID}_0.fastq.gz"
+	echo "${FQDUMP} --gzip -Z -A $accession > ${DATADIR}/HIMP2_1/HIMP2_1-${library_ID}_0.fastq.gz";
+    done >>data.batch && \
+       	${IGGMC}/scripts/qbatch submit -n data-HIMP -q normal.c -Wj -R 1 ${DATADIR}/data.batch && \
+     	${TOUCH} data.complete;
 
-done
+else
+    echo "current" >&2;
+fi
