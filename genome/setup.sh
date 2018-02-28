1ae97cbc920a0370c5679092b2d8f22dedc3d096
biozzyn <36853158+biozzyn@users.noreply.github.com>
2018-02-27 21:25:50 -0300

    Update setup.sh

--- genome/setup.sh
+++ genome/setup.sh
@@ -3,6 +3,7 @@
 # Check that external tools are accessible:
 ZCAT=`which zcat`;
 WGET=`which wget`;
+SED=`which sed`;
 
 if [[ -z "${ZCAT}" || ! -x "${ZCAT}" ]]; then
     error 127 "zcat not in PATH env variable or not executable";
@@ -10,12 +11,15 @@ if [[ -z "${ZCAT}" || ! -x "${ZCAT}" ]]; then
 elif [[ -z "${WGET}" || ! -x "${WGET}" ]]; then
     error 127 "wget not in PATH env variable or not executable";
 
+elif [[ -z "${SED}" || ! -x "${SED}" ]]; then
+    error 127 "sed not in PATH env variable or not executable";
+
 fi
 
-wget ftp://ftp.ncbi.nlm.nih.gov/sra/wgs_aux/NK/XS/NKXS01/NKXS01.1.fsa_nt.gz
+${WGET} -c ftp://ftp.ncbi.nlm.nih.gov/sra/wgs_aux/NK/XS/NKXS01/NKXS01.1.fsa_nt.gz
 
-wget ftp://ftp.ncbi.nlm.nih.gov/sra/wgs_aux/NK/XS/NKXS01/NKXS01.2.fsa_nt.gz
+${WGET} -c ftp://ftp.ncbi.nlm.nih.gov/sra/wgs_aux/NK/XS/NKXS01/NKXS01.2.fsa_nt.gz
 
-zcat NKXS01.1.fsa_nt.gz NKXS01.2.fsa_nt.gz | sed -re 's/^>(\S+)\.[0-9]+\s+.*/>\1/' > genome.fasta
+${ZCAT} NKXS01.1.fsa_nt.gz NKXS01.2.fsa_nt.gz | ${SED} -re 's/^>(\S+)\.[0-9]+\s+.*/>\1/' > genome.fasta
 
 rm *.gz
